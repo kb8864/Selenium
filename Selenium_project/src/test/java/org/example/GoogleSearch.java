da847ff86c76f099bfa90d0ebca9331858c4d1bc
kb8864 <yt7s858864@gmail.com>
2026-01-10 18:28:52 +0900

    UI自動化検証

--- Selenium_project/src/test/java/org/example/GoogleSearch.java
+++ Selenium_project/src/test/java/org/example/GoogleSearch.java
@@ -7,12 +7,32 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.Collections;
 
 public class GoogleSearch {
   public static void main(String[] args) throws InterruptedException {
+
+    // オプションを作成
+    ChromeOptions options = new ChromeOptions();
+
+    // 「自動テストソフトウェアによって制御されています」というバーを非表示にする
+    options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+    options.setExperimentalOption("useAutomationExtension", false);
+
+    // 自動化の検出フラグ（navigator.webdriver）を隠す【重要】
+    options.addArguments("--disable-blink-features=AutomationControlled");
+
+    // オプションを適用してドライバを起動
     //Chromeブラウザを立ち上げ、Googleのトップページへ移動
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver = new ChromeDriver(options);
     driver.get("https://www.google.com/");
+
+    new WebDriverWait(driver, Duration.ofSeconds(5)).until(
+            ExpectedConditions.presenceOfElementLocated(By.className("gLFyf"))
+    );
+    WebElement inputElement = driver.findElement(By.className("gLFyf"));
+    inputElement.sendKeys("Selenium" + Keys.ENTER);
 
     //すぐブラウザが閉じないように検索結果が出た状態で 10秒間）何もしないで停止し、その後ブラウザを完全に閉じる
     Thread.sleep(10000);
